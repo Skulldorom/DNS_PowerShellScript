7c4dd5f4dbe1eede91428ddaafd2f8ee4902df68
Skulldorom <51134009+Skulldorom@users.noreply.github.com>
2022-05-16 10:29:15 +0300

    Updated to a toggle file

--- ChangeToCustomDNS.ps1
+++ ChangeToCustomDNS.ps1
@@ -1,26 +1,55 @@
 param([switch]$Elevated)
 
+# The adapter we are changing the DNS settings for
+$adapter = "Ethernet"
+# Change these to use a different DNS
+$dns = "1.1.1.1", "1.0.0.1"
+
 function Test-Admin {
     $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
     $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 }
 
-if ((Test-Admin) -eq $false)  {
+if ((Test-Admin) -eq $false) {
     if ($elevated) {
         # tried to elevate, did not work, aborting
-    } else {
+    }
+    else {
         Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
     }
     exit
 }
-# Set to cloudflare dns
-Set-DnsClientServerAddress -InterfaceAlias Ethernet -ServerAddresses "1.1.1.1","1.0.0.1" # <- Put custom dns here
+function CustomDNS {
+    # Set custom DNS
+    Set-DnsClientServerAddress -InterfaceAlias $adapter -ServerAddresses $dns[0], $dns[1] # custom dns
 
-ipconfig /all # Shows your ip config
+    ipconfig /all # Shows your ip config
+    Write-Host ''
+    Write-Host '__________________________________________'
+    Write-Host ''
+    Write-Host "    - Custom DNS set to $dns -"
+    Write-Host '__________________________________________'
+    Write-Host ''
+}
 
-Write-Host ' _____  _   _  _____    _____      _   '
-Write-Host '|  __ \| \ | |/ ____|  / ____|    | |  '
-Write-Host '| |  | |  \| | (___   | (___   ___| |_ '
-Write-Host '| |  | | . ` |\___ \   \___ \ / _ \ __|'
-Write-Host '| |__| | |\  |____) |  ____) |  __/ |_ '
-Write-Host '|_____/|_| \_|_____/  |_____/ \___|\__|'
+function DefaultDNS {
+    Set-DnsClientServerAddress -InterfaceAlias Ethernet -ResetServerAddresses
+
+    ipconfig /all # Shows your ip config
+    Write-Host ''
+    Write-Host '__________________________________________'
+    Write-Host ''
+    Write-Host "     - DNS setting reset to default -"
+    Write-Host '__________________________________________'
+    Write-Host ''
+}
+
+$current = Get-DnsClientServerAddress -InterfaceAlias Ethernet -AddressFamily IPv4 | Select-Object -ExpandProperty ServerAddresses
+
+if ( $current[0] -eq $dns[0] -And $current[1] -eq $dns[1] ) {
+    DefaultDNS
+}
+else {
+    CustomDNS
+}
+
--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # Change DNS scripts
+
 ## About
 
 Used to change between a custom DNS and your Default DNS.
 
 The custom DNS uses the IPv4 settings for cloudflare
 By default it changes the ethernet settings
+
+## Getting Started
+
+- In a new powershell run `Get-NetAdapter`
+- Find the name of the adapter that you want to change the DNS for
+- Go to ChangeToCustomDNS.ps1 and put the correct adapter name in the `$adapter` variable
+- If you want to use a different DNS other than cloudflare then change the `$dns` variable
 
 ## How to run
 
@@ -23,9 +31,11 @@ In case you want it to work via a double click
 
 - Copy the full path to your PS1 script file.
 - Right click the empty space on your Desktop. Select New - Shortcut from the context menu.
-In the shortcut target box type the following, replacing `<location of script>` with the directory to your script eg.C:\Users\UserName\Documents\DefaultDNS.ps1
+  In the shortcut target box type the following, replacing `<location of script>` with the directory to your script eg.C:\Users\UserName\Documents\DefaultDNS.ps1
+
 ```
 powershell.exe -noexit -ExecutionPolicy Bypass -File <location of script>
 ```
+
 - Give it a useful name
 - Complete!
